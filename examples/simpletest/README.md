eb973caaa735c5696f8f2f4a5bc89d5dbfe45491
Erik Demaine <edemaine@mit.edu>
2018-03-28 17:34:01 -0400

    Example demo

---
+++ examples/simpletest/README.md
@@ -0,0 +1,16 @@
+# Simple test example with 2 rooms
+
+## Input spreadsheet:
+
+![Google Sheets screenshot](simpletest_gsheets.png)
+
+[.xlsx file](simpletest.xlsx) &bull;
+[Google Sheets link](https://docs.google.com/spreadsheets/d/1H486uvCgG6UELDbBFyrXODFlopZaSXFhsJL7e9gxklI/edit?usp=sharing)
+
+## Output level of Recursed:
+
+![Recursed screenshot of start room](simpletest1.png)
+
+![Recursed screenshot of key room](simpletest2.png)
+
+[Lua level](simpletest.lua)
---
+++ examples/simpletest/simpletest.lua
@@ -0,0 +1,45 @@
+local wip = { s = "solid", l = "ledge", w = "water", a = "acid" }
+function start()
+  ApplyTiles(wip, 0, 0, [[
+ssssssssssssssssssss
+s...........s......s
+s...........s......s
+s...........s......s
+s...........s......s
+s...........s......s
+s...........s......s
+s...........s......s
+s...........s......s
+s...........s......s
+s..................s
+s..................s
+s..................s
+ssssssssssssssssssss
+ssssssssssssssssssss
+]])
+  Spawn("lock", 12.5, 11.5)
+  Spawn("player", 2.5, 12)
+  Spawn("chest", 8.5, 12, "key")
+  Spawn("crystal", 16.5, 12)
+end
+function key()
+  ApplyTiles(wip, 0, 0, [[
+ssssssssssssssssssss
+s..................s
+s..................s
+s..................s
+s..................s
+s..................s
+s..................s
+s..................s
+s..................s
+s..................s
+s..................s
+s..................s
+s..................s
+ssssssssssssssssssss
+ssssssssssssssssssss
+]])
+  Spawn("player", 2.5, 12)
+  Spawn("key", 4.5, 12)
+end
